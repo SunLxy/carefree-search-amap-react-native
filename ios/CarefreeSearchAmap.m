1f89629fe9e2ce7e5f5ce6708cf4c8c81787b5de
SunLxy <1011771396@qq.com>
2022-09-23 10:57:25 +0800

    fix:修改ios类型

--- ios/CarefreeSearchAmap.m
+++ ios/CarefreeSearchAmap.m
@@ -74,7 +74,7 @@ RCT_EXPORT_METHOD(getLatLong:(NSString *) address)
 }
 
 
-RCT_EXPORT_METHOD(getAddress: (CGFloat *)latitude ,(CGFloat *)longitude)
+RCT_EXPORT_METHOD(getAddress: (CGFloat *)latitude (CGFloat *)longitude)
 {
      NSLog( @"getAddress-->" );
     AMapReGeocodeSearchRequest *regeo = [[AMapReGeocodeSearchRequest alloc] init];
